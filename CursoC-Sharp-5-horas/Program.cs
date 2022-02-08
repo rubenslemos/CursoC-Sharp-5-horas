595983c8fad7b71b77a17593f313a76d52fc8fd4
Rubens <rubenslemos@gmail.com>
2022-02-07 23:32:43 -0300

    Switch com condições

--- CursoC-Sharp-5-horas/Program.cs
+++ CursoC-Sharp-5-horas/Program.cs
@@ -12,6 +12,15 @@ namespace CursoC_Sharp_5_horas
         {
             int velocidade=0;
             bool boxes=true;
+            SwitchCase();
+            SwitchCase();
+            SwitchCase();
+            SwitchCase();
+            SwitchCase();
+            SwitchCase();
+            SwitchCase();
+            SwitchCase();
+            SwitchCase();
             string [] familia = new string[] { " rubens", " iara", " lili", " chicao", " antonio", " dinda" };
             WriteLine(familia[0] + familia[1]+familia[2]+familia[3]+familia[4]+familia[5]);
             string fim = (Condicional(velocidade, boxes));
@@ -36,6 +45,45 @@ namespace CursoC_Sharp_5_horas
             Write(condicao);
             WriteLine("");
             return fim;
+        }
+        static void SwitchCase()
+        {
+            int nota;
+            int prova = 20;
+            Write("Qual a sua nota final: ");
+            nota = int.Parse(ReadLine());
+            switch (nota)
+            {
+                case int n when (n <= 40):
+                    WriteLine("Muito burro");
+                    break;
+                case int n when (n> 40 && n <= 50):
+                    WriteLine("burro");
+                    break;                
+                case int n when (n> 50 && n <= 59):
+                    WriteLine("Faltou estudar um pouco");
+                    break;
+                case int n when (n == 60):
+                    WriteLine("Ajudado pelo professor, certeza!");
+                    break;
+                case int n when (n> 60 && n <= 69):
+                    WriteLine("Passou por pouco");
+                    break;
+                case int n when (n> 69 && n <= 79):
+                    WriteLine("Não fez mais que a obrigação");
+                    break;
+                case int n when (n> 79 && n <= 89):
+                    WriteLine("Mandou bem, Parabéns");
+                    break;
+                case int n when (n> 89 && n <= 99):
+                    WriteLine("Olha o geninho ai...");
+                    break;
+                case int n when (n == 100):
+                    WriteLine("CDF não tem como negar!");
+                    break;
+
+            }
+                
         }
     }
 }
