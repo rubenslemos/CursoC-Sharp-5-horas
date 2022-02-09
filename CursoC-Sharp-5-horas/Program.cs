f4df344488ce8b02af2bfbdb03cc958c1fa3eba8
Rubens <rubenslemos@gmail.com>
2022-02-08 22:40:27 -0300

    Enum e Switch juntos (criando menu)  e separados em exemplos proprios

--- CursoC-Sharp-5-horas/Program.cs
+++ CursoC-Sharp-5-horas/Program.cs
@@ -8,18 +8,62 @@ namespace CursoC_Sharp_5_horas
 {
     internal class Program
     {
+        //criação de um Enum
+
+        enum Carros 
+        { 
+            Porshe = 02,
+            Ferrari = 04,
+            Masserati = 06,
+            Lamborguini = 08,
+            Audi = 10,
+            Mercedes_Benz = 12,
+            BMW = 14,
+            McLaren = 16,
+            Aston_Martin = 18 
+        }
         static void Main(string[] args)
         {
+            Carros[] naFormula1 = new Carros[] { Carros.Ferrari, Carros.Mercedes_Benz, Carros.McLaren, Carros.Aston_Martin };
+            WriteLine("Qual carro você mais gosta: \n1"+(Carros)02 +"\n2"+(Carros)04+"\n3"+(Carros)06+"\n4"
+                +(Carros)08+"\n5"+(Carros)10+"\n6"+(Carros)12+"\n7"+(Carros)14+"\n8"+(Carros)16+"\n9"+(Carros)18);
+            int index = int.Parse(ReadLine());
+            switch (index)
+            {
+                case 1:
+                    WriteLine(Carros.Porshe);
+                    break;
+                case 2:
+                    WriteLine(Carros.Ferrari);
+                    break;
+                case 3:
+                    WriteLine(Carros.Masserati);
+                    break;
+                case 4:
+                    WriteLine(Carros.Lamborguini);
+                    break;
+                case 5:
+                    WriteLine(Carros.Audi);
+                    break;
+                case 6:
+                    WriteLine(Carros.Mercedes_Benz);
+                    break;
+                case 7:
+                    WriteLine(Carros.BMW);
+                    break;
+                case 8:
+                    WriteLine(Carros.McLaren);
+                    break;
+                default:
+                    WriteLine(Carros.Aston_Martin);
+                    break;
+            }
+            WriteLine(naFormula1[0] +"  "+ naFormula1[1] + "  " + naFormula1[2] + "  " + naFormula1[3]);
+            WriteLine((int)naFormula1[0] + "  " + (int)naFormula1[1] + "  " + (int)naFormula1[2] + "  " + (int)naFormula1[3]);
+            WriteLine((int)naFormula1[0] + (int)naFormula1[1] + (int)naFormula1[2] + (int)naFormula1[3]);
+
             int velocidade=0;
             bool boxes=true;
-            SwitchCase();
-            SwitchCase();
-            SwitchCase();
-            SwitchCase();
-            SwitchCase();
-            SwitchCase();
-            SwitchCase();
-            SwitchCase();
             SwitchCase();
             string [] familia = new string[] { " rubens", " iara", " lili", " chicao", " antonio", " dinda" };
             WriteLine(familia[0] + familia[1]+familia[2]+familia[3]+familia[4]+familia[5]);
@@ -49,7 +93,6 @@ namespace CursoC_Sharp_5_horas
         static void SwitchCase()
         {
             int nota;
-            int prova = 20;
             Write("Qual a sua nota final: ");
             nota = int.Parse(ReadLine());
             switch (nota)
